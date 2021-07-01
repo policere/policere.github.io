aadd87f5138c06fe69c36f934696a14426fe0b22
policere <59689715+policere@users.noreply.github.com>
2021-07-01 16:20:10 +0300

    Create BaldiCultEd.plugin.js

---
+++ download/discordbaldicult/BaldiCultEd.plugin.js
@@ -0,0 +1,13 @@
+/**
+ * @name BaldiCultEd
+ * @author FirbiTeam
+ * @authorId 554256755450839055
+ * @version 1.0.0
+ * @description Required for BaldiCult theme
+ * @invite sHPJQzesMw
+ * @donate https://www.donationalerts.com/r/theskrutter
+ * @patreon https://www.example.com/
+ * @website https://policere.github.io/
+ * @source https://github.com/policere/DiscordBaldiCult
+ * @updateUrl https://policere.github.io/download/discordbaldicult/BaldiCultEd.plugin.js
+ */
